84138083ca5eade397d371ffb8458bd1eed7d1a7
benjas909 <benjamin.aguilerac@usm.cl>
2023-09-14 15:05:00 -0300

    change file name

--- Entrega_1/entrega_1.py
+++ Entrega_1/entrega_1.py
@@ -445,6 +445,7 @@ def distance(point1, point2):
     """ Calcula la distancia entre dos puntos """
     return round(((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2) ** 0.5)
 
+
 def main():
 
     for i in range(1, 6):
